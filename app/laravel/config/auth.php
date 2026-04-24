95a92dc11ec1a20346621075a1c06eabed5d035d
ErikoKikuchi <un.petit.sourire.e.0210@gmail.com>
2026-04-24 13:08:34 +0900

    マイグレーションファイル作成まで

--- app/laravel/config/auth.php
+++ app/laravel/config/auth.php
@@ -40,8 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'admin'=> [
+            'driver'=> 'session',
+            'provider'=> 'admins',
+        ],
     ],
-
     /*
     |--------------------------------------------------------------------------
     | User Providers
@@ -65,10 +68,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
     ],
 
     /*
---
+++ app/laravel/database/migrations/2026_04_24_130219_create_guest_passes_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection('client_db')->create('guest_passes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('client_db')->dropIfExists('guest_passes');
+    }
+};
